095b839baa38cc5a7b0bdd4a6a665d78193ce526
KamilJanda <kamiljanda@yahoo.pl>
2019-03-09 23:05:48 +0100

    done

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,22 +1,46 @@
 import ply.lex as lex
 
-tokens = (
-    'ID',
-    'DOTADD', 'DOTSUB', 'DOTMUL', 'DOTDIV',
-    'ADDASSIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN',
-    'EYE', 'ZEROS', 'ONES',
-    'INTNUM', 'FLOATNUM'
-)
+literals = ['+', '-', '*', '/', '(', ')', '[', ']', '{', '}', '=', '<', '>', ':', '\'', ',', ';']
 
-literals = ['+', '-', '*', '/', '(', ')', '=']
+reserved = {
+    'if': 'IF',
+    'else': 'ELSE',
+    'for': 'FOR',
+    'while': 'WHILE',
+    'break': 'BREAK',
+    'continue': 'CONTINUE',
+    'return': 'RETURN',
+    'eye': 'EYE',
+    'zeros': 'ZEROS',
+    'ones': 'ONES',
+    'print': 'PRINT',
+}
+
+tokens = ["REALNUM", "INTNUM", 'ID', 'STRING',
+          "DOTADD", "DOTSUB", "DOTMUL", "DOTDIV",
+          "ADDASSIGN", "SUBASSIGN", "MULASSIGN", "DIVASSIGN",
+          "LEQ", "GEQ", "NEQ", "EQ",
+          ] + list(reserved.values())
+
+t_DOTADD = r'\.\+'
+t_DOTSUB = r'\.-'
+t_DOTMUL = r'\.\*'
+t_DOTDIV = r'\./'
+t_ADDASSIGN = r'\+='
+t_SUBASSIGN = r'-='
+t_MULASSIGN = r'\*='
+t_DIVASSIGN = r'/='
+t_LEQ = r'<='
+t_GEQ = r'>='
+t_NEQ = r'!='
+t_EQ = r'=='
+
+t_ignore = '  \t'
 
 
 def t_ID(t):
     r'[A-Z]'
     return t
-
-
-t_ZEROS = r'zeros'
 
 
 def t_INTNUM(t):
@@ -25,7 +49,21 @@ def t_INTNUM(t):
     return t
 
 
-t_ignore = '  \t'
+def T_REALNUM(t):
+    r"([0-9]+)(\.)([0-9]+)?|(\.)([0-9]+)"
+    t.value = float(t.value)
+    return t
+
+
+def t_COMMENT(t):
+    r'\#.*'
+    pass
+
+
+def t_STRING(t):
+    r"\"([^\\']+|\\'|\\\\)*\""
+    t.value = t.value[1:-1]
+    return t
 
 
 def t_newline(t):
@@ -40,7 +78,8 @@ def t_error(t):
 
 
 def find_column(text, tok):
-    return text.find(str(tok.value)) + 1
+    line_start = text.rfind('\n', 0, tok.lexpos) + 1
+    return (tok.lexpos - line_start) + 1
 
 
 lexer = lex.lex()
